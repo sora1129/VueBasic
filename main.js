e07e188867e4a710bfbe6acb91b0b33690c133dc
sora1129 <64789798+sora1129@users.noreply.github.com>
2022-03-23 12:29:38 +0900

    Vue.jsの基本機能 テキストのバインディング v-for/v-on/v-model/v-if/transition

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 var app = new Vue({
-	el: '#app'
+	el: '#app',
+	data: {
+		show: true,
+		message: 'Hello Vue.js',
+		list: ['りんご','ばなな','いちご'],
+		handleClick: function(e) {
+			alert(e.target)
+		}
+	}
 })
+
+console.log(app.show);
