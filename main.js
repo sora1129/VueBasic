cc880eeccd8bc24acc616135646517d87fdde859
sora1129 <64789798+sora1129@users.noreply.github.com>
2022-05-19 21:35:54 +0900

    chapter1

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 var app = new Vue({
 	el: '#app',
-	data: {
-		show: true,
-		message: 'Hello Vue.js',
-		list: ['りんご','ばなな','いちご'],
-		handleClick: function(e) {
-			alert(e.target)
-		}
-	}
+  data: {
+    message: 'Hello Vue.js',
+    list: ['りんご', 'バナナ', 'いちご']
+  },
+  methods: {
+    handleClick: function (event) {
+      alert(event.target) // [object HTMLButtonElement]
+    }
+  }
 })
