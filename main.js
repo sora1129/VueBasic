8e5fee91b0cdc31c8b6e47f6b5488164cc7fb072
sora1129 <64789798+sora1129@users.noreply.github.com>
2022-05-20 23:52:32 +0900

    条件分岐

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ var app = new Vue({
   data: {
     title: 'Hello Vue.js',
     list: ['りんご', 'バナナ', 'いちご'],
-    message: 'バインドメッセージ'
+    message: 'バインドメッセージ',
+    show: true
   },
   methods: {
     handleClick: function (event) {
