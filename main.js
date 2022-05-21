e1f90e8edf2ac8cecc2d99123ed00e6030f683b9
sora1129 <64789798+sora1129@users.noreply.github.com>
2022-05-21 17:54:07 +0900

    ctaper2 section8 # オブジェクトや配列要素の表示

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
-var app = new Vue({
-	el: '#app',
+// CHAPTER1
+// var app = new Vue({
+// 	el: '#app',
+//   data: {
+//     title: 'Hello Vue.js',
+//     list: ['りんご', 'バナナ', 'いちご'],
+//     message: 'バインドメッセージ',
+//     show: true
+//   },
+//   methods: {
+//     handleClick: function (event) {
+//       alert(event.target) // [object HTMLButtonElement]
+//     }
+//   }
+// })
+
+var app = new Vue ({
+  el: '#app',
   data: {
-    title: 'Hello Vue.js',
-    list: ['りんご', 'バナナ', 'いちご'],
-    message: 'バインドメッセージ',
-    show: true
-  },
-  methods: {
-    handleClick: function (event) {
-      alert(event.target) // [object HTMLButtonElement]
-    }
+    message: {
+      value: 'Hello Vue.js!'
+    },
+    list: ['りんご', 'ばなな', 'いちご'],
+    num: 1
   }
 })
