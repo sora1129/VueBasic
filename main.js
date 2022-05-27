d611de4128be46a38b3d882ffeb0d726d312f855
sora1129 <64789798+sora1129@users.noreply.github.com>
2022-05-27 10:08:10 +0900

    // リストデータの表示と更新 // 要素を繰り返し描画する

--- main.js
+++ main.js
@@ -1,79 +1,12 @@
-// CHAPTER1
-// var app = new Vue({
-// 	el: '#app',
-//   data: {
-//     title: 'Hello Vue.js',
-//     list: ['りんご', 'バナナ', 'いちご'],
-//     message: 'バインドメッセージ',
-//     show: true
-//   },
-//   methods: {
-//     handleClick: function (event) {
-//       alert(event.target) // [object HTMLButtonElement]
-//     }
-//   }
-// })
-
-// var app = new Vue ({
-//   el: '#app',
-//   data: {
-//     message: {
-//       value: 'Hello Vue.js!'
-//     },
-//     list: ['りんご', 'ばなな', 'いちご'],
-//     num: 1
-//   }
-// })
-
-// var app = new Vue ({
-//   el: '#app',
-//   data: {
-//     message: 'Hello Vue.js!',
-//     scroll: 0
-//   },
-
-// })
-
-// section8
-/*
+// リストデータの表示と更新
+// 要素を繰り返し描画する
 new Vue ({
   el: '#app',
   data: {
-    count: 0
-  },
-  methods: {
-    // ボタンをクリックしたときのハンドラ
-    increment: function() {
-      this.count++ 
-    }
-  }
-})*/
-
-/*
-// クラスとスタイルのデータバインディング
-new Vue({
-  el: '#app',
-  data: {
-    isChild: true,
-    isActive: true,
-    textColor: 'red',
-    bgColor: 'lightblue'
-  }
-})*/
-
-// SVGのデータバインディング
-// new Vue({
-//   el: '#app',
-//   data: {
-//     radius: 50
-//   }
-// })
-
-// テンプレートにおける条件分岐
-new Vue({
-  el: '#app',
-  data: {
-    ok: false,
-    type: 'A'
+    list: [
+      { id: 1, name: 'スライム', hp: 100 },
+      { id: 2, name: 'ゴブリン', hp: 200 },
+      { id: 3, name: 'ドラゴン', hp: 500 }
+    ]
   }
 })
